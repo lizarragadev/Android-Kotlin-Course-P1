659156e36696d1309fcff252cf9764f402f70e9e
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2023-06-06 06:37:13 -0400

    Add gitignore

---
+++ _00_KotlinExamples/src/main/kotlin/k_01_tiposDeDatoYEntradaPorTeclado/Ejemplo1.kt
@@ -0,0 +1,14 @@
+package k_01_tiposDeDatoYEntradaPorTeclado
+
+/*
+* VAR: Variable mutable
+* VAL: Variable inmutable *USAR MAS*
+* */
+val x: Int = 4
+val y = 6
+// var se reservan 2 espacios de memoria
+// val se reserva 1 espacio de memoria
+fun main() {
+    val suma = x + y
+    println(suma)
+}
