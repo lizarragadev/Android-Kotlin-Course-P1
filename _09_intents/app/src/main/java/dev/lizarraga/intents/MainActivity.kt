f10ba2a6d58de06ee502be2271514fb8f6b32841
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2023-06-26 20:18:35 -0400

    Intents demo

--- _09_intents/app/src/main/java/dev/lizarraga/intents/MainActivity.kt
+++ _09_intents/app/src/main/java/dev/lizarraga/intents/MainActivity.kt
@@ -1,11 +1,54 @@
 package dev.lizarraga.intents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import dev.lizarraga.intents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ibAbrirActivity.setOnClickListener{
+            abrirActivity()
+        }
+
+        binding.ibEnviarDatos.setOnClickListener {
+            enviarDatos()
+        }
+
+        binding.ibDevolverDatos.setOnClickListener {
+            devolverDatos()
+        }
+    }
+
+    private fun abrirActivity(){
+        startActivity(Intent(this, SegundaActivity::class.java))
+    }
+
+    private fun enviarDatos(){
+        val intent = Intent(this, SegundaActivity::class.java)
+        intent.putExtra("valor1", "Hola desde MainActivity")
+        intent.putExtra("valor2", 87 )
+        startActivity(intent)
+    }
+
+    private fun devolverDatos() {
+        val intent = Intent(this, SegundaActivity::class.java)
+        intent.putExtra("valor3", "Mi nombre es...")
+        resultDevolverDatos.launch(intent)
+    }
+
+    val resultDevolverDatos = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val resultado = result.data?.getStringExtra("respuesta")
+            Toast.makeText(this, resultado, Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- _09_intents/app/src/main/java/dev/lizarraga/intents/SegundaActivity.kt
+++ _09_intents/app/src/main/java/dev/lizarraga/intents/SegundaActivity.kt
@@ -1,11 +1,31 @@
 package dev.lizarraga.intents
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.lizarraga.intents.databinding.ActivitySegundaBinding
 
 class SegundaActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySegundaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_segunda)
+        binding = ActivitySegundaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bundle = intent.extras
+
+        if(bundle != null) {
+            binding.tvTextoUno.text = bundle.getString("valor1", "")
+            binding.tvTextoDos.text = bundle.getInt("valor2", 0).toString()
+            binding.etCajaTexto.setText(bundle.getString("valor3", ""))
+        }
+        binding.btnRespuesta.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra("respuesta", binding.etCajaTexto.text.toString())
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
     }
 }
