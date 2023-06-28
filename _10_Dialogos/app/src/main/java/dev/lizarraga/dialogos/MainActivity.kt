9e66db0ce7a388142212751c140673109972da0e
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2023-06-28 17:12:00 -0400

    Final code

--- _10_Dialogos/app/src/main/java/dev/lizarraga/dialogos/MainActivity.kt
+++ _10_Dialogos/app/src/main/java/dev/lizarraga/dialogos/MainActivity.kt
@@ -2,8 +2,10 @@ package dev.lizarraga.dialogos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import dev.lizarraga.dialogos.databinding.ActivityMainBinding
+import dev.lizarraga.dialogos.databinding.CustomDialogBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -32,19 +34,51 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun crearDialogoSimple() : AlertDialog {
-
-        return null!!
+        val alertDialog = AlertDialog.Builder(this)
+        alertDialog
+            .setTitle("Mi Dialogo")
+            .setMessage("Este es el contenido del dialogo")
+            .setCancelable(false)
+            .setPositiveButton("Aceptar") { a, d ->
+                Toast.makeText(this, "Aceptado", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("Rechazar") { a, d ->
+                Toast.makeText(this, "Rechazado", Toast.LENGTH_SHORT).show()
+            }
+            .setNeutralButton("Cancelar") { a, d ->
+                Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show()
+            }
+        return alertDialog.create()
     }
 
     fun crearDialogoConLista(): AlertDialog {
-
-        return null!!
+        val alertDialog = AlertDialog.Builder(this)
+        val opciones = arrayOf("Opción 1", "Opción 2", "Opción 3")
+        alertDialog
+            .setTitle("Selecciona una opción")
+            .setCancelable(false)
+            .setItems(opciones) { a, i ->
+                Toast.makeText(this, "Click en $i", Toast.LENGTH_SHORT).show()
+            }
+            .setNeutralButton("Cancelar") { a, d ->
+                Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show()
+            }
+        return alertDialog.create()
     }
 
     fun crearDialogoConListaRadio(): AlertDialog {
         val alertDialog = AlertDialog.Builder(this)
-        val opciones = arrayOf("Femenino", "Masculino", "Sin especificar")
         var posicion = 0
+        val opciones = arrayOf("Femenino", "Masculino", "Sin especificar")
+        alertDialog
+            .setTitle("Dialogo lista radio")
+            .setCancelable(false)
+            .setSingleChoiceItems(opciones, 0) { a, i ->
+                posicion = i
+            }
+            .setPositiveButton("Aceptar") { a, d ->
+                Toast.makeText(this, "Seleccionado $posicion", Toast.LENGTH_SHORT).show()
+            }
 
         return alertDialog.create()
     }
@@ -53,12 +87,42 @@ class MainActivity : AppCompatActivity() {
         val alertDialog = AlertDialog.Builder(this)
         val opciones = arrayOf("Android", "iOS", "Java", "Kotlin")
         var itemSeleccionados = ArrayList<Int>()
-
+        
+        alertDialog
+            .setTitle("Un dialogo con lista checkbox")
+            .setCancelable(false)
+            .setMultiChoiceItems(opciones, null) { a, i, b ->
+                if (b) {
+                    itemSeleccionados.add(i)
+                } else  {
+                    if(itemSeleccionados.contains(i)) {
+                        itemSeleccionados.remove(i)
+                    }
+                }
+            }
+            .setPositiveButton("Aceptar") { a, d ->
+                var texto = ""
+                for (i in itemSeleccionados) {
+                    texto += "$i "
+                }
+                Toast.makeText(this, "Seleccionados $texto", Toast.LENGTH_SHORT).show()
+            }
 
         return alertDialog.create()
     }
 
     fun crearDialogoPersonalizado() {
-
+        val bind = CustomDialogBinding.inflate(layoutInflater)
+        val alertDialog = AlertDialog.Builder(this)
+        alertDialog.setCancelable(false)
+        alertDialog.setView(bind.root)
+        val dialog = alertDialog.show()
+        bind.btnCrear.setOnClickListener {
+            dialog.dismiss()
+        }
+        bind.btnEntrar.setOnClickListener {
+            Toast.makeText(this, "Bienvenido ${bind.etNombre.text}", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
     }
 }
--- _11_notificaciones/app/src/main/java/dev/lizarraga/otificaciones/MainActivity.kt
+++ _11_notificaciones/app/src/main/java/dev/lizarraga/otificaciones/MainActivity.kt
@@ -1,11 +1,77 @@
 package dev.lizarraga.otificaciones
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.app.NotificationCompat
+import dev.lizarraga.otificaciones.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var binding: ActivityMainBinding
+    lateinit var mNotificationManager: NotificationManager
+    private val CANAL_ID = "miCanal01"
+    private val CANAL_NOMBRE = "Mi canal de notificaciones"
+    private val CANAL_DESCRIPCION = "Este es un canal para mis notificaciones..."
+    private val NOTIFICATION_ID = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mNotificationManager = applicationContext
+            .getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importancia = NotificationManager.IMPORTANCE_DEFAULT
+            val canal = NotificationChannel(CANAL_ID, CANAL_NOMBRE, importancia)
+            canal.description = CANAL_DESCRIPCION
+            canal.enableLights(true)
+            canal.enableVibration(true)
+            canal.lockscreenVisibility = NotificationManager.IMPORTANCE_DEFAULT
+            mNotificationManager.createNotificationChannel(canal)
+        }
+
+        binding.btnNotificacionSimple.setOnClickListener(this)
+        binding.btnNotificacionGrande.setOnClickListener(this)
+        binding.btnNotificacionImagenGrande.setOnClickListener(this)
+        binding.btnNotificacionAcciones.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.btnNotificacionSimple -> crearNotificacionSimple()
+            R.id.btnNotificacionGrande -> crearNotificacionTextoGrande()
+            R.id.btnNotificacionImagenGrande -> crearNotificacionImagenGrande()
+            R.id.btnNotificacionAcciones -> crearNotificacionConAcciones()
+        }
+    }
+
+    private fun crearNotificacionConAcciones() {
+        TODO("Not yet implemented")
+    }
+
+    private fun crearNotificacionImagenGrande() {
+        TODO("Not yet implemented")
+    }
+
+    private fun crearNotificacionTextoGrande() {
+        TODO("Not yet implemented")
+    }
+
+    private fun crearNotificacionSimple() {
+        val notificacion = NotificationCompat.Builder(this, CANAL_ID)
+        notificacion
+            .setContentTitle("Titulo de notificacion")
+            .setContentText("Este es el texto de la notificacion")
+            .setSmallIcon(android.R.drawable.ic_dialog_alert)
+            .setLargeIcon(BitmapFactory.decodeResource(this.resources, android.R.drawable.ic_dialog_email))
+
+        mNotificationManager.notify(NOTIFICATION_ID, notificacion.build())
     }
 }
