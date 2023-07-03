9521fe54469a308cc5b015fe96a6cdfacb111658
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2023-07-03 15:36:01 -0400

    base code

---
+++ _12_widgets/app/src/main/java/dev/lizarraga/widgets/ui/PickersActivity.kt
@@ -0,0 +1,12 @@
+package dev.lizarraga.widgets.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.lizarraga.widgets.R
+
+class PickersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pickers)
+    }
+}
